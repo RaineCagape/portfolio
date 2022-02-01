3ed7bf330e18c323e644a76daf2d9ba2dd143247
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2022-02-01 14:02:27 +0800

    Adjust Router links

--- src/Main.js
+++ src/Main.js
@@ -2,7 +2,7 @@ import './css/Main.css';
 import './css/mobile.css';
 import React,{ useState }  from 'react';
 import {
-  BrowserRouter as Router,
+  HashRouter as Router,
   Switch,
   Route,
   Link,
@@ -70,9 +70,9 @@ export default function CustomLinkExample() {
 const NavMob = () => (
   <div className='nav-list-mob'> 
                  <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio'>Intro</a></p>
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/info'>About</a></p>
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/skills'>Skills</a></p>
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/works'>Works</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/portfolio/info'>About</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/portfolio/skills'>Skills</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/portfolio/works'>Works</a></p>
                  <div className="social-bar-mob">
                   <a href="https://www.linkedin.com/in/reyna-elaine-cagape-a13803179/" target="_blank"><FontAwesomeIcon className="linkedin" icon={faLinkedin}/></a>
                   <a href="https://github.com/RaineCagape" target="_blank"><FontAwesomeIcon className="github" icon={faGithub} /></a>
