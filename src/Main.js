0612c912fe7c278239f51981943318072802d3e0
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2022-02-01 15:34:42 +0800

    Update Main.js

--- src/Main.js
+++ src/Main.js
@@ -64,6 +64,7 @@ export default function CustomLinkExample() {
               </Route>
             </Switch>
       </div>  
+      
     </Router>
   );
 }
@@ -71,10 +72,10 @@ export default function CustomLinkExample() {
 const NavMob = () => (
   
   <div className='nav-list-mob'> 
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio' target="_top">Intro</a></p>
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/info' target="_top">About</a></p>
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/skills' target="_top">Skills</a></p>
-                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/works' target="_top">Works</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio' target="_top" rel="opener">Intro</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/info' target="_top" rel="opener">About</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/skills' target="_top" rel="opener">Skills</a></p>
+                 <p className="list" ><FontAwesomeIcon className="arrow" icon={faCaretRight} /><a href='https://rainecagape.github.io/portfolio/#/works' target="_top" rel="opener">Works</a></p>
                  <div className="social-bar-mob">
                   <a href="https://www.linkedin.com/in/reyna-elaine-cagape-a13803179/" target="_blank"><FontAwesomeIcon className="linkedin" icon={faLinkedin}/></a>
                   <a href="https://github.com/RaineCagape" target="_blank"><FontAwesomeIcon className="github" icon={faGithub} /></a>
