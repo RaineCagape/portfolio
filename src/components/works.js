a653a9b9406f5574ac8fe7270f76709cd799e454
RaineCagape <31531825+RaineCagape@users.noreply.github.com>
2024-01-15 20:54:55 +0800

    remove a work

--- src/components/works.js
+++ src/components/works.js
@@ -35,12 +35,12 @@ class Works extends Component {
                 </div>
               </div>
               <div className='second-set'>
-                <div className='work'>
+                {/* <div className='work'>
                   <a href="https://rainelandpage.x10.mx/" target="_blank">
                     <img className='web-img lp' src={slp} />
                   </a>
-                  <a className="works highlight" href="https://rainelandpage.x10.mx/" target="_blank" >Sample Landing Page</a>
-                </div>
+                  <a className="works highlight" href="https://rainelandpage.x10.mx/" target="_blank" >Sample Wordpress Site Installation</a>
+                </div> */}
                 <div className='work'>
                    <a  href="https://westpointeheights.com/" target="_blank">
                     <img className='web-img lps' src={wh} />
@@ -67,12 +67,12 @@ class Works extends Component {
                </div>
              </div>
              <div className="second-set-mob">
-               <div className='work'>
+               {/* <div className='work'>
                  <a href="https://rainelandpage.x10.mx/" target="_blank">
                    <img className='web-img lp' src={slp} />
                  </a>
-                 <a className="works highlight" href="https://rainelandpage.x10.mx/" target="_blank" >Sample Landing Page</a>
-               </div>
+                 <a className="works highlight" href="https://rainelandpage.x10.mx/" target="_blank" >Sample Wordpress Site Installation</a>
+               </div> */}
                <div className='work'>
                   <a  href="https://westpointeheights.com/" target="_blank">
                    <img className='web-img lps' src={wh} />
