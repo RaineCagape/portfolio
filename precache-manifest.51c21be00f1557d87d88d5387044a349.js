ddce82dcc81ece386ee96fc1f5cee1fc448ce824
RaineCagape <rainecagape@gmail.com>
2024-12-28 23:35:57 +0800

    Updates

---
+++ precache-manifest.51c21be00f1557d87d88d5387044a349.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebfb01ae2225d183c975bce97f7dd7cb",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "1d1204bb2e49995a09df",
+    "url": "/portfolio/static/css/2.dbb74888.chunk.css"
+  },
+  {
+    "revision": "a6e626d7185ba967ed78",
+    "url": "/portfolio/static/css/main.8dc65fad.chunk.css"
+  },
+  {
+    "revision": "1d1204bb2e49995a09df",
+    "url": "/portfolio/static/js/2.9b9ac340.chunk.js"
+  },
+  {
+    "revision": "dfe6c94fa43cb2c2ca76dff59f1f8be5",
+    "url": "/portfolio/static/js/2.9b9ac340.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a6e626d7185ba967ed78",
+    "url": "/portfolio/static/js/main.6ed89a32.chunk.js"
+  },
+  {
+    "revision": "0d4bf47b70941af9967b",
+    "url": "/portfolio/static/js/runtime-main.8940859c.js"
+  },
+  {
+    "revision": "62f3dff78244acb8ca0f89e610854ed9",
+    "url": "/portfolio/static/media/cosmarketing.62f3dff7.png"
+  },
+  {
+    "revision": "7bc37a88147859c3b1056faab6792577",
+    "url": "/portfolio/static/media/fl_logo.7bc37a88.gif"
+  },
+  {
+    "revision": "40bb48c174011488f4ff6072fa0bd16a",
+    "url": "/portfolio/static/media/intermed.40bb48c1.png"
+  },
+  {
+    "revision": "4439ec02568b8e91ee9aab68a06ee778",
+    "url": "/portfolio/static/media/intro-bg.4439ec02.jpg"
+  },
+  {
+    "revision": "cc99b3a108cab7df9400167bc138af59",
+    "url": "/portfolio/static/media/ipassonlinerev.cc99b3a1.png"
+  },
+  {
+    "revision": "bc80fa15816f13456b5ae029751c001e",
+    "url": "/portfolio/static/media/iw.bc80fa15.png"
+  },
+  {
+    "revision": "94d3d10636cc687a589ceede61776ca2",
+    "url": "/portfolio/static/media/lhh.94d3d106.png"
+  },
+  {
+    "revision": "fec52db3dc83e6884be425e45c817958",
+    "url": "/portfolio/static/media/lpscannr.fec52db3.png"
+  },
+  {
+    "revision": "24242686a91b7e30a147d11a3869dc59",
+    "url": "/portfolio/static/media/p4p.24242686.png"
+  },
+  {
+    "revision": "ba1656f6faa3d8fa15ba56786ab647fe",
+    "url": "/portfolio/static/media/project-d.ba1656f6.png"
+  },
+  {
+    "revision": "cf5608122d93b6574cb2de5d2f2d9005",
+    "url": "/portfolio/static/media/rec.cf560812.jpg"
+  },
+  {
+    "revision": "6d839721ef69d775ec00649b7c32cd0e",
+    "url": "/portfolio/static/media/westpointeheights.6d839721.png"
+  }
+]);
